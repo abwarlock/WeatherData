8cd0e9f97f5558d0b7aa2f53e00a47620b657acb
abdevWarlock <abdevwarlock@gmail.com>
2019-02-28 06:58:23 +0530

    Grouping Files

--- app/src/main/java/com/abdev/weatherdata/activties/MainActivity.kt
+++ app/src/main/java/com/abdev/weatherdata/activties/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.abdev.weatherdata
+package com.abdev.weatherdata.activties
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.abdev.weatherdata.R
 
 class MainActivity : AppCompatActivity() {
 
